f37ad126bbb1a081e53cb62d62f561edb313281c
Maulik Patel <maulik.patel.cg@gmail.com>
2026-09-23 15:45:17 +0530

    Create Topic-6 — String Slicing.py

---
+++ python_unit_2(sem_1)/Assignment/Assignment 3/Topic-6 — String Slicing.py
@@ -0,0 +1,71 @@
+#Question-24 -----------------------
+
+text = "PYTHON"
+print(text[0:3]) #Output = PYT
+print(text[2:5]) #Output = THO
+print(text[1:6]) #Output = YTHON
+
+#Question-25 ---------------------
+
+text = "PROGRAMMING"
+print(text[:4]) #PROG
+print(text[4:]) #RAMMING
+print(text[:]) #PROGRAMMING
+
+#Question-26 -------------------
+
+text = "COMPUTER"
+print(text[-5:]) #Output = PUTER
+print(text[:-3]) #Output = COMPU
+print(text[-6:-2]) #Output = MPUT
+
+#Question-27 -------------------
+
+text = "PYTHON"
+print(text[::2]) #Output = PTO
+print(text[1::2]) #Output = YHN
+print(text[::-1]) #Output = NOHTYP 
+
+#Question-28 --------------------
+text-1= "Python"
+print(text [::-1]) #output = nohtyP
+
+text-2 = "Hello"
+print(text[::-1]) #Output = olleH
+
+text-3 = 12345
+print(text-3 [::-1]) #Output = 54321
+
+#Question-29 ------------------
+
+text-1 = "ABCDEFGH"
+print(text-1 [::2])
+
+text-2 = "Python"
+print(text-2 [::2])
+
+text-3 = 12345678
+print(text-3 [::2])
+
+#Question-30 -------------------
+
+words = ["Programming", "Computer", "Python"]
+
+first_three = words[:3]  
+print(first_three)
+last_three = words[-3:] 
+print(last_three)
+
+#Question-31 -------------------
+
+text = "ABCDEFGHIJ"
+print(text[2:8:2]) #Output = CEG
+print(text[8:2:-2]) #Output = IGE
+print(text[::-2]) #Output = JHFDB
+
+#Question-32 ------------------------
+
+text = "BTECH-CSE-2026"
+print(text [0:5]) 
+print(text [6:9])
+print(text [10:14])
