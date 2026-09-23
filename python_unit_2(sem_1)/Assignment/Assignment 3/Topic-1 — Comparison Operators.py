88ddf39c6b3b4aa606a909ce410d926a52bab242
Maulik Patel <maulik.patel.cg@gmail.com>
2026-09-23 15:33:03 +0530

    Update Topic-1 — Comparison Operators.py

--- python_unit_2(sem_1)/Assignment/Assignment 3/Topic-1 — Comparison Operators.py
+++ python_unit_2(sem_1)/Assignment/Assignment 3/Topic-1 — Comparison Operators.py
@@ -1,1 +1,38 @@
+#Question-1 ---------------------------------------
 
+a = 15
+b = 20
+
+print(a < b) # output = True
+print(a > b) # output = False
+print(a == b) # output = False
+print(a != b) # output = True
+print(a <= b) # output = True
+print(a >= b) # output = False
+
+#Question-2 --------------------------------------
+
+x = 10
+y = 10
+
+print(x == y) # output = True
+print(x != y) # output = False
+print(x < y) # output = False
+print(x <= y) # output = True
+print(x >= y) # output = True
+
+#Question-3 ------------------------------------
+
+a = 10
+b = 5
+
+print(a + b == 15) #output = True
+print(a * b > 40)  #output = True
+print(a - b != 5)  #output = False
+print(a // b == 2)  #output = True
+
+#Question-4 ------------------------------------
+
+print("Python" == "Python") #output = True
+print("Python" == "python") #output = False
+print("Hello" != "hello") #output = True
