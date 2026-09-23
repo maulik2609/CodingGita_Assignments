d8617c4545616ef6c8072f2a08a96932c6f5173d
Maulik Patel <maulik.patel.cg@gmail.com>
2026-09-23 15:49:27 +0530

    Create Topic-10 — Debugging.py

---
+++ python_unit_2(sem_1)/Assignment/Assignment 3/Topic-10 — Debugging.py
@@ -0,0 +1,26 @@
+#Question-54 --------------------------
+
+age = int(input("Enter age: "))
+print("Age after 5 years:", age + 5)
+
+#Question-55 --------------------------
+
+print("It's Python")
+
+#Question-56 --------------------------
+
+text = "Python"
+print(text[1:4])
+
+#Question-57 -------------------------
+
+a,b = input().split(",")
+
+#Question-58 -----------------------
+
+a, b = input().split() #input = 10 20
+print(int(a) + int(b)) #Output = 30
+
+#Question-59 --------------------
+
+print("C:\\new\\test")
