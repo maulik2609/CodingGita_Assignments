4e5e712d4627c59da071339d6ccbfbdece6c66f0
Maulik Patel <maulik.patel.cg@gmail.com>
2026-09-23 15:48:47 +0530

    Create Topic-9 — print(), sep, end, and f-Strings.py

---
+++ python_unit_2(sem_1)/Assignment/Assignment 3/Topic-9 — print(), sep, end, and f-Strings.py
@@ -0,0 +1,28 @@
+#Question-49 -----------------------
+
+print("2026", "09", "09", sep="-") #Output = 2026-09-09
+
+#Question-50 ----------------------
+
+print("Hello", end=" ")
+print("Python")      #Output = Hello Python
+
+#Question-51 --------------------
+
+print(10, 20, 30, sep='-')
+print(40, 50, 60, sep='-')
+
+#Question-52 -----------------------
+
+Name = input("Name :")
+Age = input("Age :")
+City = input("City :")
+Course = input("Course :")
+
+print(f"Name : {Name}\n Age : {Age}\n City : {City}\n Course : {Course}")
+
+#Question-53 --------------------------
+
+price = float(input("Enter the price: "))
+
+print(f"{price:.2f}")
