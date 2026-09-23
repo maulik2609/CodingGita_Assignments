4d77bc5c0a1f2db7aafa49ef4b7d9db6d1ece271
Maulik Patel <maulik.patel.cg@gmail.com>
2026-09-23 15:44:24 +0530

    Add string indexing examples in Python

---
+++ python_unit_2(sem_1)/Assignment/Assignment 3/Topic-5 — String Indexing.py
@@ -0,0 +1,46 @@
+#Question-19 ------------------
+
+text="PYTHON"
+print(text[0]) #Output = P
+print(text[1]) #Output = Y
+print(text[5]) #Output = N
+print(text[4]) #Output = O
+
+#Question-20 --------------------
+
+text= "COMPUTER"
+print(text[0]) #Output = C
+print(text[3]) #Output = P
+print(text[-1]) #Output = R
+print(text[-3]) #Output = T
+
+#Question-21 -------------------
+
+text = "PYTHON"
+
+print(text[0]) #Output = P
+print(text[2]) #Output = T
+print(text[-1]) #Output = N
+print(text[-2]) #Output = O
+
+#Question-22 -----------------
+
+text-1= "Python"
+print(text-1 [0]) #Output = P
+print(text-1 [5]) #Output = n
+
+text-2= "Computer"
+print(text-2 [0]) #Output = C
+print(text-2 [7]) #Output = r
+
+text-3= "Hello"
+print(text-3 [0]) #Output = H
+print(text-3 [4]) #Output = o
+
+#Question-23 ----------------------
+
+word = "PROGRAM"
+print(word[0]) #Output = P
+print(word[2]) #Output = O
+print(word[-1]) #Output = M
+print(word[-4]) #Output = G
