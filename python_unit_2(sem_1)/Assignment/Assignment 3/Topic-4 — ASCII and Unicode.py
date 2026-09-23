7765d2038bb8caa5e6c1fa595e60b1e93a85ae0e
Maulik Patel <maulik.patel.cg@gmail.com>
2026-09-23 15:43:48 +0530

    Create Topic-4 — ASCII and Unicode.py

---
+++ python_unit_2(sem_1)/Assignment/Assignment 3/Topic-4 — ASCII and Unicode.py
@@ -0,0 +1,49 @@
+#Question-12 -----------------------------------
+
+print(ord("A")) #output = 65 
+print(ord("a")) #output = 97 
+print(ord("Z")) #output = 90 
+print(ord("z")) #output = 122
+print(ord("0")) #output = 48 
+print(ord("9")) #output = 57 
+print(ord("@")) #output = 64
+
+#Question-13 --------------------------------
+
+print(chr(65)) #output = A
+print(chr(66)) #output = B
+print(chr(97)) #output = a
+print(chr(98)) #output = b
+print(chr(48)) #output = 0
+print(chr(57)) #output = 9
+print(chr(64)) #output = @
+
+#Question-14 -----------------------------
+
+print(ord("A")) #Output = 65
+print(ord("a")) #Output = 97
+print(ord("B")) #Output = 66
+print(ord("b")) #Output = 98
+
+#1. Large among these two is ord("a")
+#2. Difference Between them is 32 .
+#3. Yes Difference Between the B and b is also 32 .
+
+#Question-17 ------------------------
+A=65 
+B=66
+a=97
+b=98
+0=48
+9=57
+
+print("A" < "B") #Output = True
+print("a" < "b") #Output = True
+print("A" < "a") #Output = True
+print("0" < "9") #Output = True
+
+#Question-18 ---------------------
+
+print(chr(9731)) #Output =☃
+print(chr(9829)) #Output =♥
+print(chr(8377)) #Output =₹
