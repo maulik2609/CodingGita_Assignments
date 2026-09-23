190bbafc687e80678023a8e61938297b0b60fbeb
Maulik Patel <maulik.patel.cg@gmail.com>
2026-09-23 15:43:03 +0530

    Create Topic-3 — Membership Operators with Strings.py

---
+++ python_unit_2(sem_1)/Assignment/Assignment 3/Topic-3 — Membership Operators with Strings.py
@@ -0,0 +1,35 @@
+#Question-7 -----------------------
+
+text = "Python Programming"
+
+print("Python" in text) #Output = True
+print("Java" in text) #Output =  False
+print("Python" not in text) #Output =  False
+
+#Question-8 ----------------------
+
+word = "computer"
+print("p" in word) #Output = True
+print("x" in word)  #Output = True
+print("c" in word)  #Output = True
+
+#Question-9 ----------------------
+
+text = "Python"
+ 
+print("P" in text) #output = True
+print("p" in text) #output = False
+print("Python" in text) #output = True
+print("python" in text) #output = False
+
+#Question-10 -----------------------
+
+text= "apple , banana"  
+print("apple" in text) #output = True
+print("python" in text) #output = False
+print("banana" in text) #output = True
+
+#Question-11 ------------------------
+
+text= "rahul@gmail.com, student@yahoo.com, rahulgmail.com"
+print("@" in text) #output =
