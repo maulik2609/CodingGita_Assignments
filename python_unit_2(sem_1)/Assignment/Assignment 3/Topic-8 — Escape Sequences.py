f042d81a2fc0ad31d8f51ea7ff95aef5dabd31bf
Maulik Patel <maulik.patel.cg@gmail.com>
2026-09-23 15:46:34 +0530

    Create Topic-8 — Escape Sequences.py

---
+++ python_unit_2(sem_1)/Assignment/Assignment 3/Topic-8 — Escape Sequences.py
@@ -0,0 +1,29 @@
+#Question-42 -------------------------
+print("Hello \n World") #Output = Hello
+                                # World
+
+#Question-43 ------------------------
+
+print("Name:\tRahul")
+print("Age:\t20")
+print("City:\tAhmedabad")
+
+#Question-44 ---------------------
+
+print("C:\\Python\\Programs")
+
+#Question-45 ------------------
+print("It's a Python")
+
+#Question-46 -----------------
+
+print("He Said \"Hello\"")
+
+#Question-47  ------------------
+
+print("Python\nProgramming") #Output = Python
+                                    # Programming
+
+#Question-48 ---------------------
+
+print("Student Detail\n\n Name: \tRahul\n Age: \t20\n Course: B.Tech")
